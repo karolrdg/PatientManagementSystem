57036696d63397364d8eec15bafffd01e9b6abb2
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-11-06 15:18:57 -0300

    feat(components): implement SubmitButton l className

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
-const SubmitButton = () => {
-  return (
-    <div>SubmitButton</div>
-  )
+interface ButtonProps {
+  isLoading: boolean;
+  className?: string;
+  children: React.ReactNode;
 }
 
-export default SubmitButton
+const SubmitButton = ({
+  isLoading,
+  className,
+  children,
+}: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      disabled={isLoading}
+      className={className ?? "shad-primary-btn w-full"}
+    >
+      {isLoading ? (
+        <div className="flex items-center gap-4">
+          <Image
+            src="/assets/icons/loader.svg"
+            alt="loader"
+            width={24}
+            height={24}
+            className="animate-spin"
+          />
+          Loading...
+        </div>
+      ) : (
+        children
+      )}
+    </Button>
+  );
+};
+
+export default SubmitButton;
