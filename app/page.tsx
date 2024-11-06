1b2cb8a33a569083b20a1872e857a91e29b83d02
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-11-06 15:18:35 -0300

    fix(layout): remove background color from main container for improved styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="flex h-screen max-h-screen bg-slate-600">
+    <div className="flex h-screen max-h-screen">
       <section className="remove-scroll-bar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
