8ee4d8eff5cdcd572efe043dcf74b8a471cb9b58
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-11-07 17:30:40 -0300

    feat(config): update Appwrite configuration to use collection IDs for patients, doctors, and appointments

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,17 +1,17 @@
 import * as sdk from "node-appwrite";
 
 export const {
-    NEXT_PUBLIC_ENDPOINT: ENDPOINT,
-    PROJECT_ID,
-    API_KEY,
-    DATABASE_ID,
-    PATIENT_ID,
-    DOCTOR_ID,
-    APPOINTMENT_ID,
-    NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-  } = process.env;
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENT_COLLECTION_ID,
+  DOCTOR_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+} = process.env;
 
-  const client = new sdk.Client();
+const client = new sdk.Client();
 
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
