a3e9e5f74c23c81687623835b9897a6d5dd26f9e
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-11-11 15:58:39 -0300

    feat(actions): add getUser function to retrieve user details

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -2,8 +2,10 @@
 import { Query, ID } from "node-appwrite";
 //import { parseStringify } from "../utils"
 import { users } from "../appwrite.config"
+import { parseStringify } from "../utils";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//CREATE
 export const createUser = async (user: CreateUserParams) => {
     try {
       const newUser = await users.create(
@@ -21,5 +23,19 @@ export const createUser = async (user: CreateUserParams) => {
         const documents = await users.list([Query.equal("email", [user.email])]);
         return documents?.users[0];
       }
+    }
+  };
+
+  //GET
+  export const getUser = async (userId: string) => {
+    try {
+      const user = await users.get(userId);
+  
+      return parseStringify(user);
+    } catch (error) {
+      console.error(
+        "An error occurred while retrieving the user details:",
+        error
+      );
     }
   };
