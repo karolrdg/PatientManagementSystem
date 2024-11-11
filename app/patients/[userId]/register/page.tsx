db2381a442bb694015cca60ce552d511726cac57
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-11-11 10:53:14 -0300

    feat(register): create Register component for patient registration page

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+
+const Register = () => {
+  return (
+    <div className="flex h-screen max-h-screen">
+    <section className="remove-scroll-bar container my-auto">
+      <div className="sub-container max-w-[500px]">
+        <Image
+          src={"/assets/icons/logo-full.svg"}
+          height={1000}
+          width={1000}
+          alt="logo"
+          className="mb-8 h-10 w-fit"
+        />
+        {/*PatientForm />*/}
+        <div className="text-14-regular mt-20 flex justify-between">
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 CarePulse
+          </p>
+          <Link
+            href="/?adm=true"
+            className="text-green-500"
+          >
+            Admin Login
+          </Link>
+        </div>
+      </div>
+    </section>
+
+    <Image
+      src="/assets/images/register-img.png"
+      height={1000}
+      width={1000}
+      alt="doctor"
+      className="side-img max-w-[390px]"
+    />
+  </div>
+  );
+};
+
+export default Register;
