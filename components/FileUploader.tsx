8c0d98300daaaae602a23a566e132ec760b4e7f4
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-11-18 16:52:33 -0300

    feat(file-uploader): implement drag-and-drop functionality in FileUploader component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 
-const FileUploader = () => {
+type FileUploaderProps = {
+    files: File[] | undefined;
+    onChange: (files: File[]) => void;
+    }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const FileUploader = ({ files, onChange} : FileUploaderProps) => {
+  const onDrop = useCallback((acceptedFiles : File[]) => {
+    onChange(acceptedFiles)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div>FileUploader</div>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          // eslint-disable-next-line react/no-unescaped-entities
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
   )
 }
 
-export default FileUploader
+export default FileUploader;
