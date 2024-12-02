14e32b61be7352256f39928ebb853bf72c90f7f2
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-12-02 17:50:06 -0300

    fix(patient): remove duplicate getPatient function and clean up code

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -56,6 +56,7 @@ export const getUser = async (userId: string) => {
   }
 };
 
+// GET PATIENT
 export const getPatient = async (userId: string) => {
   try {
       const patients = await databases.listDocuments(
@@ -109,20 +110,4 @@ export const registerPatient = async ({
   }
 };
 
-// GET PATIENT
-export const getPatient = async (userId: string) => {
-  try {
-    const patients = await databases.listDocuments(
-      DATABASE_ID!,
-      PATIENT_COLLECTION_ID!,
-      [Query.equal("userId", [userId])]
-    );
 
-    return parseStringify(patients.documents[0]);
-  } catch (error) {
-    console.error(
-      "An error occurred while retrieving the patient details:",
-      error
-    );
-  }
-};
